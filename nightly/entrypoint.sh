0b416129ffcf938b59a1875f741dc867ad4af433
Minhchau Dang <minhchau.dang@liferay.com>
2017-12-08 16:34:27 -0800

    Allow extra setup script to be added which is run before the bundle is started

--- nightly/entrypoint.sh
+++ nightly/entrypoint.sh
@@ -2,6 +2,13 @@
 
 . /home/liferay/common.sh $1
 
+if [ -f ${LIFERAY_HOME}/setup.sh ]; then
+	cd ${LIFERAY_HOME}
+	chmod u+x setup.sh
+	./setup.sh
+	cd -
+fi
+
 if [ "true" == "${IS_UPGRADE}" ]; then
 	LIFERAY_HOME=${LIFERAY_HOME} /home/liferay/upgrade.sh
 else
