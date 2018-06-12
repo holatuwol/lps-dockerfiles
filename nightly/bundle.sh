fbcc107e9e07893fa69f773c821bc5bd0e7d5e60
Minhchau Dang <minhchau.dang@liferay.com>
2018-06-12 15:49:16 -0700

    Allow specifying the heap size

--- nightly/bundle.sh
+++ nightly/bundle.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Starting Liferay"
+
+if [ "" == "${JVM_HEAP_SIZE}" ]; then
+	JVM_HEAP_SIZE='2g'
+fi
+
+sed -i.bak "s/-Xms[^ ]*/-Xms${JVM_HEAP_SIZE}/g" ${LIFERAY_HOME}/tomcat/bin/setenv.sh
+sed -i.bak "s/-Xmx[^ ]*/-Xmx${JVM_HEAP_SIZE}/g" ${LIFERAY_HOME}/tomcat/bin/setenv.sh
+
 JPDA_ADDRESS='0.0.0.0:8000' ${LIFERAY_HOME}/tomcat/bin/catalina.sh jpda run
--- nightly/upgrade.sh
+++ nightly/upgrade.sh
@@ -2,5 +2,12 @@
 
 echo "Starting upgrade..."
 
+if [ "" == "${JVM_HEAP_SIZE}" ]; then
+	JVM_HEAP_SIZE='8g'
+fi
+
+mkdir -p ${LIFERAY_HOME}/osgi/configs
+echo 'indexReadOnly=true' > ${LIFERAY_HOME}/osgi/configs/ com.liferay.portal.search.configuration.IndexStatusManagerConfiguration.cfg
+
 cd ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/
-java -jar com.liferay.portal.tools.db.upgrade.client.jar -j "-Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx8g"
+java -jar com.liferay.portal.tools.db.upgrade.client.jar -j "-Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx${JVM_HEAP_SIZE}"
