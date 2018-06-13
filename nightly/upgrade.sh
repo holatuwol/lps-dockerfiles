d1c8ff4dbfbe2f469265c5634d51e120530a39c5
Minhchau Dang <minhchau.dang@liferay.com>
2018-06-13 16:55:49 -0700

    Auto-generate upgrade tool properties files from portal-ext.properties

--- nightly/upgrade.sh
+++ nightly/upgrade.sh
@@ -7,7 +7,21 @@ if [ "" == "${JVM_HEAP_SIZE}" ]; then
 fi
 
 mkdir -p ${LIFERAY_HOME}/osgi/configs
-echo 'indexReadOnly=true' > ${LIFERAY_HOME}/osgi/configs/ com.liferay.portal.search.configuration.IndexStatusManagerConfiguration.cfg
+echo 'indexReadOnly=true' > ${LIFERAY_HOME}/osgi/configs/com.liferay.portal.search.configuration.IndexStatusManagerConfiguration.cfg
+
+touch ${LIFERAY_HOME}/.liferay-home
+
+echo "dir=${LIFERAY_HOME}/tomcat
+extra.lib.dirs=/bin
+global.lib.dir=/lib
+portal.dir=/webapps/ROOT
+server.detector.server.id=tomcat" > ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/app-server.properties
+
+cat ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/app-server.properties
+
+grep -F 'jdbc.default' ${LIFERAY_HOME}/portal-ext.properties > ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/portal-upgrade-database.properties
+grep -vF 'jdbc.default' ${LIFERAY_HOME}/portal-ext.properties | grep . > ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/portal-upgrade-ext.properties
+echo "liferay.home=${LIFERAY_HOME}" >> ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/portal-upgrade-ext.properties
 
 cd ${LIFERAY_HOME}/tools/portal-tools-db-upgrade-client/
-java -jar com.liferay.portal.tools.db.upgrade.client.jar -j "-Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx${JVM_HEAP_SIZE}"
+java -Duser.dir=$PWD -jar com.liferay.portal.tools.db.upgrade.client.jar -j "-Dfile.encoding=UTF8 -Duser.timezone=GMT -Xmx${JVM_HEAP_SIZE}"
