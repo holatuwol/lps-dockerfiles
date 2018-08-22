fae0506c683ad45fceee62640916cb5b69a59e77
Minhchau Dang <minhchau.dang@liferay.com>
2018-08-21 17:38:12 -0700

    Add TCPPING initial hosts values on all JDKs where JDBCPING is not set

--- nightly-jdk8/setenv.sh
+++ nightly-jdk8/setenv.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ -f /opt/liferay/tcp.xml ] && [ "" != "$(grep -F TCPPING /opt/liferay/tcp.xml)" ]; then
+  BASE_IP=$(hostname -I | cut -d'.' -f 1,2,3)
+  INITIAL_HOSTS=$(seq 255 | awk '{ print "'${BASE_IP}'." $1 "[7800],'${BASE_IP}'." $1 "[7801]" }' | tr '\n' ',' | sed 's/,$//g')
+  CATALINA_OPTS="${CATALINA_OPTS} -Djgroups.tcpping.initial_hosts='${INITIAL_HOSTS}'"
+fi
+
 CATALINA_OPTS="${CATALINA_OPTS} -Dfile.encoding=UTF-8 -Djava.net.preferIPv4Stack=true -Duser.timezone=GMT -Xms${JVM_HEAP_SIZE} -Xmx${JVM_HEAP_SIZE} -XX:MaxMetaspaceSize=1g -XX:MetaspaceSize=1g"
