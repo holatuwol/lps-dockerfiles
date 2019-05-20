121115a01d832d75f0ec8ff901940ee0fdb053e5
Minhchau <minhchau.dang@liferay.com>
2019-05-20 08:49:27 -0700

    Add missing dockerfiles

---
+++ elasticsearch65/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE_NAME=$1
+DOCKERHUB_USER=$2
+
+if [ "" == "${IMAGE_NAME}" ]; then
+	IMAGE_NAME=liferay-elasticsearch
+fi
+
+if [ "" == "${DOCKERHUB_USER}" ]; then
+	DOCKERHUB_USER=holatuwol
+fi
+
+docker build . -t "${DOCKERHUB_USER}/${IMAGE_NAME}:6.5"
+docker push "${DOCKERHUB_USER}/${IMAGE_NAME}:6.5"
