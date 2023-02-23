733409aeb33cc7420617f1742a2aa3822179ffff
Minhchau <holatuwol@gmail.com>
2023-02-23 15:47:01 -0800

    Make sure properties go into the properties folder in the patch

--- create-dummy-hotfix-61/scripts/prepare_hotfix.sh
+++ create-dummy-hotfix-61/scripts/prepare_hotfix.sh
@@ -11,6 +11,13 @@ copy_ext_files WAR_PATH/WEB-INF/lib ext-lib/portal
 copy_ext_files GLOBAL_LIB_PATH/portal-service.jar ext-service/classes
 copy_ext_files WAR_PATH/WEB-INF/lib/portal-impl.jar ext-impl/classes
 
+mkdir /patch/properties
+
+for file in $(find /patch/jdk6/WAR_PATH/WEB-INF/lib/portal-impl.jar -name '*.properties' | sed 's@^.*/portal-impl.jar/@@g'); do
+  mkdir -p /patch/properties/$(dirname "${file}")
+  mv /patch/jdk6/WAR_PATH/WEB-INF/lib/portal-impl.jar/${file} /patch/properties/${file}
+done
+
 copy_ext_files WAR_PATH/WEB-INF/lib/util-bridges.jar ext-util-bridges/classes
 copy_ext_files WAR_PATH/WEB-INF/lib/util-java.jar ext-util-java/classes
 copy_ext_files WAR_PATH/WEB-INF/lib/util-taglib.jar ext-util-taglib/classes
