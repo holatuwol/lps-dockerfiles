d9a3d5f105b6a2b35855866e1917b53483dc0975
Minhchau <holatuwol@gmail.com>
2023-10-04 17:25:45 +0900

    Run Liferay on JDK6 from zip file and patches rather than from a folder

--- run-liferay-jdk6/entrypoint.sh
+++ run-liferay-jdk6/entrypoint.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-if [ ! -d /bundles/tomcat-${TOMCAT_VERSION}/bin ]; then
-  echo 'docker run --rm -v ${LIFERAY_HOME}:/bundles -p 8080:8080 -p 8000:8000 holatuwol/run-liferay:jdk6'
-  exit 1
+cd /
+
+if [ -f liferay-portal-tomcat-6.1.10-*.zip ]; then
+	rm -rf liferay-portal-6.1.10-*
+	unzip -qq liferay-portal-tomcat-6.1.10-*.zip
+	cd liferay-portal-6.1.10-*
+elif [ -f liferay-portal-tomcat-6.1.20-*.zip ]; then
+	rm -rf liferay-portal-6.1.20-*
+	unzip -qq liferay-portal-tomcat-6.1.20-*.zip
+	cd liferay-portal-6.1.20-*
 fi
 
-/bundles/tomcat-${TOMCAT_VERSION}/bin/catalina.sh jpda run
+rm -rf patching-tool
+unzip -qq ../patching-tool*.zip
+
+test -d /patches && cp /patches/*.zip patching-tool/patches/
+sed -i 's@PT_OPTS=@echo PT_OPTS=@g' patching-tool/patching-tool.sh
+cd patching-tool
+
+chmod u+x patching-tool.sh
+./patching-tool.sh auto-discovery
+./patching-tool.sh install
+
+mkdir -p ../deploy
+cp /license.xml ../deploy/
+
+if [ -f /portal-setup-wizard.properties ]; then
+	cp /portal-setup-wizard.properties ../
+else
+	echo -n 'setup.wizard.enabled=false' > ../portal-setup-wizard.properties
+fi
+
+cd ../tomcat*/bin
+
+./catalina.sh jpda run
